b7d826803258c497ea5667258a7970d6a9d35f0d
shalini2807-7 <shalinipvijay@gmail.com>
2026-02-04 22:57:14 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const mybutton = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
